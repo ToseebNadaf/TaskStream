7469bee37f5b08410896b8a095f197be3b38422b
ToseebNadaf <toseebnadaf@gmail.com>
2024-12-22 23:00:33 +0530

    Created Navbar & Authentication form component

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/navbar";
+import UserAuthForm from "./pages/userAuthForm";
+
+const App = () => {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Navbar />}>
+          <Route path="signin" element={<UserAuthForm type="sign-in" />} />
+          <Route path="signup" element={<UserAuthForm type="sign-up" />} />
+        </Route>
+      </Routes>
+    </>
+  );
+};
+
+export default App;
