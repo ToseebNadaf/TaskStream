3ab0739deb53291e1d34d30e4168becf91a86d7c
ToseebNadaf <toseebnadaf@gmail.com>
2024-12-24 21:48:14 +0530

    Refactor: Update middleware file

--- backend/middlewares/Error.js
+++ backend/middlewares/Error.js
@@ -1,12 +1,16 @@
-const ErrorMiddleware = (err, req, res) => {
+const ErrorMiddleware = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
-
   err.message = err.message || "Internal Server Error";
 
-  res.status(err.statusCode).json({
-    success: false,
-    message: err.message,
-  });
+  if (res && typeof res.status === "function") {
+    return res.status(err.statusCode).json({
+      success: false,
+      message: err.message,
+    });
+  } else {
+    console.error("Response object is missing or invalid");
+    return next(err);
+  }
 };
 
 export default ErrorMiddleware;
